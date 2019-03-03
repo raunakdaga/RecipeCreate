d8ef49bb15eb468773e3d13cf225898fb5fb8871
Raunak <raunakdaga@gmail.com>
2019-03-03 00:07:42 -0500

    Uploaded images / files

---
+++ getImage.py
@@ -0,0 +1,22 @@
+import io
+import os
+from google.cloud import vision
+from google.cloud.vision import types
+
+client = vision.ImageAnnotatorClient()
+
+file_name = os.path.join(os.path.dirname(__file__), 'bike.jpg')
+
+with io.open(file_name, 'rb') as image:
+    content = image.read()
+
+image = types.Image(content=content)
+
+response = client.label_detection(image=image)
+
+labels = response.label_annotations
+
+print('Labels: ')
+
+for label in labels:
+    print(label)
